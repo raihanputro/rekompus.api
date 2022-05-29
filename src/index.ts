eb02aeb83bd3f4784cd4587d7f6c106fdb36d729
nnunggaran <040273034@ecampus.ut.ac.id>
2022-05-29 22:58:08 +0700

    tambah packages untuk jwt & route login

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from "express"
+import helmet from "helmet"
 import cors from "cors"
 import "reflect-metadata"
+import * as bodyParser from "body-parser"
+
+import loginRoutes from "./routes/login"
 
 function main(){
   const port = 3000
@@ -9,8 +13,14 @@ function main(){
   app.use(cors({
     origin: '*'
   }))
-
+  app.use(helmet())
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
+  
   app.get("/", (req, res) => res.send("Rekompus API Server"))
+  
+  app.use('/login', loginRoutes)
+
 
   app.listen(port, () => {
     console.log(`[server] server running at http://localhost:${port} ⚡`)
